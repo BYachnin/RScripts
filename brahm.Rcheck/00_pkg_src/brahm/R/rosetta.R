16f0d45ebf0e2f942b3d4a0514c6419657fe291f
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2016-10-10 20:11:18 -0400

    Revert "Cleaned up some stuff related to making the package"
    This reverts commit d79a9b9019fa63a10d0052a37e1b601540e5df2a.

---
+++ brahm.Rcheck/00_pkg_src/brahm/R/rosetta.R
@@ -0,0 +1,18 @@
+#Some scripts to deal with output from Rosetta.
+
+#Load in functions to read files from disk.
+source("https://raw.githubusercontent.com/BYachnin/RScripts/master/io.R")
+
+#Plot a graph of two score terms from a score file.
+#If the axes are not given, assume they are total_score vs. rmsd.
+ros_plot <- function(datatable, xterm = "rmsd", yterm = "total_score") {
+  graphics.off()
+  plot(datatable[,xterm], datatable[,yterm], xlab = xterm, ylab = yterm)
+}
+
+#Combine the load data and plotting functions.  Return the score file.
+score_plot <- function(scorefile, xterm = "rmsd", yterm = "total_score") {
+  scores <- read_rosetta_score(scorefile)
+  ros_plot(scores, xterm = xterm, yterm = yterm)
+  return(scores)
+}
