ace6875fa2425c5c24c48c2f007fcf0eff6aaff0
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2016-10-28 14:23:44 -0400

    Updated Rosetta scoreplot functions
    I removed the graphics.off() in the automatic score plot function to
    make it more scriptable.  Also added the option for putting titles on
    the graphs.  Trying it also with hard links in brahm/R pointing to .

--- brahm/R/rosetta.R
+++ brahm/R/rosetta.R
@@ -5,14 +5,14 @@ source("https://raw.githubusercontent.com/BYachnin/RScripts/master/io.R")
 
 #Plot a graph of two score terms from a score file.
 #If the axes are not given, assume they are total_score vs. rmsd.
-ros_plot <- function(datatable, xterm = "rmsd", yterm = "total_score") {
-  graphics.off()
-  plot(datatable[,xterm], datatable[,yterm], xlab = xterm, ylab = yterm)
+ros_plot <- function(datatable, xterm = "rmsd", yterm = "total_score", title = "") {
+  #graphics.off()
+  plot(datatable[,xterm], datatable[,yterm], xlab = xterm, ylab = yterm, main = title)
 }
 
 #Combine the load data and plotting functions.  Return the score file.
-score_plot <- function(scorefile, xterm = "rmsd", yterm = "total_score") {
+score_plot <- function(scorefile, xterm = "rmsd", yterm = "total_score", title = scorefile) {
   scores <- read_rosetta_score(scorefile)
-  ros_plot(scores, xterm = xterm, yterm = yterm)
+  ros_plot(scores, xterm = xterm, yterm = yterm, title = title)
   return(scores)
 }
