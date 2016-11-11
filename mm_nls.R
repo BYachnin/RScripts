d928676198f50f44f7f0f35f328d69b1f2ec58d1
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2016-11-10 19:09:22 -0500

    Added a menu loop for re-processing data in mm_nls.R

--- mm_nls.R
+++ mm_nls.R
@@ -4,6 +4,7 @@ closeAllConnections()
 rm(list = ls())
 library(xlsx)
 library(brahm)
+library(utils)
 source("mm_functions.R")
 
 wt_data <- read_spectramax("9-2-16WTIso_NH.xlsx", "Test2")
@@ -27,5 +28,17 @@ result_list$Dataset <- c(10, 20, 40)
 
 #Check if the data frame is numeric to do non-linear regression
 if (is.numeric(result_list$Dataset) && is.numeric(result_list$Rate)) {
-  mm_fit <- mm_nls(result_list$Dataset, result_list$Rate)
+  #Perform the non-linear regression, and then allow the user to re-process.
+  while (TRUE) {
+    #Do the non-linear regression.
+    mm_fit <- mm_nls(result_list$Dataset, result_list$Rate)
+    summary(mm_fit)
+    #Provide a menu with all of the datasets.  Re-run the linear regression for those.
+    setnumber <- menu(result_list$Dataset, title = "Select a dataset to re-process:")
+    #If the number from the menu is 0, we're done.  Break out of the while loop.
+    if (setnumber == 0) {break}
+    reg_results <- regression_loop(wt_data, colnames(wt_data[1]), colnames(wt_data[1+setnumber]))
+    #Replace the value in result_list.
+    result_list[setnumber, 2] <- reg_results[[1]]
+  }
 }
