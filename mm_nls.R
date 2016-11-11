36dacc509af9aee9718338bdc0b0169877c85124
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2016-11-10 19:36:43 -0500

    Changed mm_nls.R to include min and max time in summary table

--- mm_nls.R
+++ mm_nls.R
@@ -16,12 +16,12 @@ for (dataset in colnames(wt_data[-1])) {
   if (is.numeric(dataset)) {
     new_result <- data.frame(as.numeric(dataset), reg_results[[1]])
   } else {
-    new_result <- data.frame(dataset, reg_results[[1]])
+    new_result <- data.frame(dataset, reg_results[[1]], reg_results[[3]], reg_results[[4]])
   }
   result_list <- rbind(result_list, new_result)
 }
 
-names(result_list) <- c("Dataset", "Rate")
+names(result_list) <- c("Dataset", "Rate", "Min Time", "Max Time")
 
 #Hard code in the result_list dataset names to force it to be numeric for testing.
 result_list$Dataset <- c(10, 20, 40)
@@ -32,13 +32,17 @@ if (is.numeric(result_list$Dataset) && is.numeric(result_list$Rate)) {
   while (TRUE) {
     #Do the non-linear regression.
     mm_fit <- mm_nls(result_list$Dataset, result_list$Rate)
+    print(result_list)
     summary(mm_fit)
     #Provide a menu with all of the datasets.  Re-run the linear regression for those.
     setnumber <- menu(result_list$Dataset, title = "Select a dataset to re-process:")
     #If the number from the menu is 0, we're done.  Break out of the while loop.
     if (setnumber == 0) {break}
+    #Print out the old data.
+    print(result_list[setnumber,])
+    #Re-do linear regression.
     reg_results <- regression_loop(wt_data, colnames(wt_data[1]), colnames(wt_data[1+setnumber]))
-    #Replace the value in result_list.
-    result_list[setnumber, 2] <- reg_results[[1]]
+    #Replace the values in result_list.
+    result_list[setnumber,] <- data.frame(result_list[setnumber, 1], reg_results[[1]], reg_results[[3]], reg_results[[4]])
   }
 }
