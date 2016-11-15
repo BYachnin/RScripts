34b289cd0870355e9deb72f651c03d7faf3281b8
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2016-11-15 17:58:00 -0500

    Added ability to toggle the exclude variable
    In preparation for adding an exclude data capability in the non-linear
    regression.

--- mm_nls.R
+++ mm_nls.R
@@ -61,15 +61,28 @@ if (is.numeric(result_list$Dataset) && is.numeric(result_list$Rate)) {
     print(result_list)
     summary(mm_fit)
     #Provide a menu with all of the datasets.  Re-run the linear regression for those.
-    setnumber <- menu(result_list$Dataset, title = "Select a dataset to re-process:")
+    setnumber <- menu(c(result_list$Dataset, "Include/exclude datasets"), title = "Select a dataset to re-process (0 to quit):")
     #If the number from the menu is 0, we're done.  Break out of the while loop.
     if (setnumber == 0) {break}
-    #Print out the old data.
-    print(result_list[setnumber,])
-    #Re-do linear regression.
-    reg_results <- regression_loop(wt_data, colnames(wt_data[1]), colnames(wt_data[1+setnumber]))
-    #Replace the values in result_list.
-    result_list[setnumber,] <- data.frame(result_list[setnumber, 1], reg_results[[1]], reg_results[[3]], reg_results[[4]], FALSE)
+    #If the number from the menu is the highest value, switch to the exclude dataset mode.
+    if (setnumber == length(result_list$Dataset)+1) {
+      #Stay in include/exclude mode until the user chooses 0.
+      switchset <- 1
+      while (switchset != 0) {
+        #Get the set to toggle.
+        switchset <- menu(paste(result_list$Dataset, result_list$`Exclude?`, sep = ': '), title = "Select a dataset to toggle include/exclude state (0 to quit):")
+        #Toggle switchset.
+        result_list[switchset, 5] <- !result_list[switchset, 5]
+      }
+    } else {
+      #Otherwise, reprocess the selected datapoint.
+      #Print out the old data.
+      print(result_list[setnumber,])
+      #Re-do linear regression.
+      reg_results <- regression_loop(wt_data, colnames(wt_data[1]), colnames(wt_data[1+setnumber]), result_list[setnumber,3], result_list[setnumber,4])
+      #Replace the values in result_list.
+      result_list[setnumber,] <- data.frame(result_list[setnumber, 1], reg_results[[1]], reg_results[[3]], reg_results[[4]], FALSE)
+    }
   }
 }
 
