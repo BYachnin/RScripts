b728ce9c392298025ee10f37e97f344910f25df3
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2017-09-08 14:38:21 -0400

    Added best_designs
    This function pulls out a custom list of designs sorted based on a
    scoreterm of choice.  Flexible with direction of sorting and the first
    and last design to include.

--- brahm/R/rosetta.R
+++ brahm/R/rosetta.R
@@ -29,3 +29,23 @@ score_plot <- function(scorefile, xterm = "rmsd", yterm = "total_score", title =
   ros_plot(scores, xterm = xterm, yterm = yterm, title = title)
   return(scores)
 }
+
+#' Return a list of the top designs in a Rosetta scorefile, based on a scoreterm of choice.
+#'
+#' @details The first and last design can be customized.
+#'
+#' @inherit ros_plot datatable
+#' @param scoreterm The name of the scoreterm to be used to sort.
+#' @param top The first design to include.  For example, if it is 3, the top two designs will be skipped.
+#' @param number The total number of designs to list.
+#' @param descending If true, the highest score will be considered the "best."  If false (default), the lowest score will be considered the "best."
+#' @return A list of the design names requested.
+#' @export
+#' @seealso \code{\link{score_plot}} \code{\link{read_rosetta_score}}
+best_designs <- function(datatable, scoreterm = "total_score", top = 1, number = 50, descending = FALSE) {
+  #Sort the data according to scoreterm.
+  sortedscores <- scores[order(scores[,scoreterm], decreasing = descending),]
+  #Pull out the names of the desired designs.
+  selnames <- rownames(sortedscores[top:(top + number - 1),])
+  return(selnames)
+}
